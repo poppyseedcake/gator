17b9bb5b99ea39a8104d21844d254e83829fb4ba
poppyseedcake <w.makowiec@gmail.com>
2026-02-08 16:47:22 +0100

    set up drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "drizzle-kit";
+import { readConfig } from "src/config";
+
+
+const config = readConfig();
+const url = config.dbUrl;
+
+export default defineConfig({
+  schema: "src/lib/db/schema.ts",
+  out: "src/lib/db/",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: url,
+  },
+});
